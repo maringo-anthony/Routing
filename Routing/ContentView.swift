390097cdfee933d132832a84ae57a3293fe9389f
Anthony Maringo <alm4cu@virginia.edu>
2020-05-10 17:56:56 -0400

    Initial Commit

---
+++ Routing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Routing
+//
+//  Created by Anthony MARINGO on 5/10/20.
+//  Copyright © 2020 Anthony MARINGO. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
